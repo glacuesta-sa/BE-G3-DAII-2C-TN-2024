64191800e2429ef025ee7b86166987856aa799cb
Gastón Axel Lacuesta <glacuesta@secureauth.com>
2024-10-23 02:09:03 -0300

    fix tests

--- test/test_websocket.py
+++ test/test_websocket.py
@@ -67,54 +67,60 @@ def test_connect(setup_dynamodb):
 
 
 def test_disconnect(setup_dynamodb):
-    # Simular el evento de desconexión
-    event = {
-        'requestContext': {
-            'connectionId': 'test-connection-id'
+    with patch("api.websocket.get_conn_table") as mock_dynamodb_table:
+        mock_dynamodb_table.return_value = setup_dynamodb.Table('ConnectionsTable')
+        
+        # Simular el evento de desconexión
+        event = {
+            'requestContext': {
+                'connectionId': 'test-connection-id'
+            }
         }
-    }
 
-    # Insertar un elemento en la tabla
-    table = setup_dynamodb.Table('ConnectionsTable')
-    table.put_item(Item={'connectionId': 'test-connection-id'})
+        # Insertar un elemento en la tabla
+        table = setup_dynamodb.Table('ConnectionsTable')
+        table.put_item(Item={'connectionId': 'test-connection-id'})
 
-    response = disconnect(event, None)
+        response = disconnect(event, None)
 
-    # Verificar que el item fue eliminado de DynamoDB
-    item = table.get_item(Key={'connectionId': 'test-connection-id'})
-    assert 'Item' not in item
+        # Verificar que el item fue eliminado de DynamoDB
+        item = table.get_item(Key={'connectionId': 'test-connection-id'})
+        assert 'Item' not in item
 
-    # Verificar la respuesta
-    assert response['statusCode'] == 200
-    assert json.loads(response['body']) == 'Desconectado'
+        # Verificar la respuesta
+        assert response['statusCode'] == 200
+        assert json.loads(response['body']) == 'Desconectado'
 
 
 def test_default(setup_dynamodb, mock_apigateway):
-    # Simular el evento de mensaje
-    event = {
-        'requestContext': {
-            'connectionId': 'test-connection-id',
-            'domainName': 'example.com',
-            'stage': 'dev'
-        },
-        'body': json.dumps({
-            'message': 'Hello, WebSocket!'
-        })
-    }
+    with patch("api.websocket.get_conn_table") as mock_dynamodb_table:
+        mock_dynamodb_table.return_value = setup_dynamodb.Table('ConnectionsTable')
+        
+        # Simular el evento de mensaje
+        event = {
+            'requestContext': {
+                'connectionId': 'test-connection-id',
+                'domainName': 'example.com',
+                'stage': 'dev'
+            },
+            'body': json.dumps({
+                'message': 'Hello, WebSocket!'
+            })
+        }
 
-    response = default(event, None)
+        response = default(event, None)
 
-    # Verificar que se llama a post_to_connection en el cliente de API Gateway
-    mock_apigateway.post_to_connection.assert_called_once_with(
-        ConnectionId='test-connection-id',
-        Data=json.dumps({
-            'message': 'Se recibio el mensaje Hello, WebSocket!',
-            'connectionId': 'test-connection-id'
-        })
-    )
+        # Verificar que se llama a post_to_connection en el cliente de API Gateway
+        mock_apigateway.post_to_connection.assert_called_once_with(
+            ConnectionId='test-connection-id',
+            Data=json.dumps({
+                'message': 'Se recibio el mensaje Hello, WebSocket!',
+                'connectionId': 'test-connection-id'
+            })
+        )
 
-    # Verificar la respuesta
-    assert response['statusCode'] == 200
-    assert json.loads(response['body']) == {
-        'message': 'Se recibio el mensaje Hello, WebSocket!'
-    }
+        # Verificar la respuesta
+        assert response['statusCode'] == 200
+        assert json.loads(response['body']) == {
+            'message': 'Se recibio el mensaje Hello, WebSocket!'
+        }
